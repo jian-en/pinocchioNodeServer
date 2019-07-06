5d2e165b57ee895de30006bd3d42610f611609b1
mia81514 <mia81514@gmail.com>
2019-07-06 11:25:55 -0400

    move generateToken and verifyToken into auth.j

--- routes/api.js
+++ routes/api.js
@@ -15,13 +15,11 @@ const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
 // jwt
-const jwt = require('jsonwebtoken');
-const { jwtSecret } = require('../config.js');
-const { checkAuth } = require('../utils/auth.js');
+const auth = require('../utils/auth.js');
 const { sendMail } = require('../utils/mailer');
 
 // Add an event
-router.post('/event', checkAuth, async (req, res, next) => {
+router.post('/event', auth.checkAuth, async (req, res, next) => {
   const { eventType, eventName } = req.body;
 
   const item = {
@@ -92,35 +90,43 @@ router.post('/login', [
   check('password').trim().isLength({min: 8}).isAlphanumeric(),
 ], async (req, res, next) => {
   // check whether inputs are valid
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({success: false, errors: errors.array()});
+  const validation = validationResult(req);
+  if (!validation.isEmpty()) {
+    const msgs = validation.errors.map(err => `The ${err.param} has incorrect format.`)
+    return res.status(422).json({success: false, errors: msgs});
   }
   // get the user
   const { email, password } = req.body;
   const ret = await dynamoDb.getUser(email);
-  if (!ret.success) return res.status(500).json(ret);
-  else if (ret.data.length == 0) return res.json({success: false, message: "The email is incorrect."});
+  if (!ret.success)
+    return res.status(500).json(ret);
+  else if (ret.data.length == 0)
+    return res.status(422).json({success: false, errors: ["The email is incorrect."]});
+
   // check password
   const user = ret.data[0];
   const hashedPwd = user.password;
   if (!bcrypt.compareSync(password, hashedPwd))
-    return res.json({success: false, message: "The password is incorrect."});
+    return res.status(422).json({success: false, message: "The password is incorrect."});
+
   // issue token
   const payload = {usersId: user.usersId, email: user.email};
-  const token = jwt.sign(payload, jwtSecret, {expiresIn: '2h'});
-  res.json({success: true, token, id: user.usersId, email: user.email});
+  const token = auth.generateToken(payload);
+  if (token) {
+    res.json({success: true, token, id: user.usersId, email: user.email});
+  } else {
+    res.status(500).json({success: false, error: "Server error when generating token."});
+  }
 });
 
 router.post('/getUser', async (req, res, next) => {
   const { token } = req.body;
-  jwt.verify(token, jwtSecret, (err, payload) => {
-    if (err) {
-      res.json({success: false, ...err})
-    } else {
-      res.json({success: true, id: payload.usersId, email: payload.email})
-    }
-  });
+  const decoded = auth.verifyToken(token);
+  if (decoded) {
+    res.json({success: true, id: decoded.usersId, email: decoded.email});
+  } else {
+    res.json({success: false, error: "Token is invalid."});
+  }
 });
 
 module.exports = router;
