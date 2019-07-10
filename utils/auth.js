32bc760bbe20ade6d96d7838567b1d038c5903a3
mia81514 <mia81514@gmail.com>
2019-07-10 15:19:52 -0400

    use json to send response for consistency

--- utils/auth.js
+++ utils/auth.js
@@ -6,11 +6,11 @@ const jwtSecret = config[env].jwtSecret;
 module.exports.checkAuth = (req, res, next) => {
   const token = req.body.token;
   if (!token) {
-    res.status(401).send('Unauthorized: No token provided');
+    res.status(401).json({success: false, message: 'Unauthorized: No token provided'});
   } else {
     jwt.verify(token, jwtSecret, function(err, payload) {
       if (err) {
-        res.status(401).send('Unauthorized: Invalid token');
+        res.status(401).json({success: false, message: 'Unauthorized: Invalid token'});
       } else {
         req.email = payload.email;
         req.usersId = payload.usersId;
