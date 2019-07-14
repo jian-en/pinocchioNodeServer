80f5bb7de6cd57d481ab4625af908b1f45d73809
Brian Wan <bwan1224@yahoo.com>
2019-07-14 18:52:36 -0400

    swaggered the app

--- server.js
+++ server.js
@@ -6,13 +6,51 @@ starts server on port 3000
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 // load app config
 const { reactServer } = require('./config.js');
 
 // create express app
 var app = express();
-var port = process.env.PORT || 3000;
+var portNumber = 3000;
+var port = process.env.PORT || portNumber;
+
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'Pinocchio node backend server',
+    version: '1.0.0',
+    description: 'API endpoints to handle Pinocchio system routes',
+  },
+  host: 'localhost:'+3010,//portNumber,
+  basePath: '/',
+  securityDefinitions: {
+    token: {
+      type: 'apiKey',
+      name: 'token',
+      scheme: 'bearer',
+      in: 'header',
+    },
+  },
+};
+
+// swagger options
+const options = {
+  swaggerDefinition,
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+// swagger docs endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // parse application/json
 app.use(bodyParser.json());
--- src/routes/accounts.router.js
+++ src/routes/accounts.router.js
@@ -5,15 +5,138 @@
 module.exports = (app) => {
     const accounts = require('../controllers/accounts.controller.js');
 
-    // create a new account
+    /**
+     * @swagger
+     * /api/accounts/register:
+     *  post:
+     *      tags:
+     *          - Accounts
+     *      name: Register
+     *      summary: Registers a new account
+     *      produces:
+     *          - application/json
+     *      consumes:
+     *          - application/x-www-form-urlencoded
+     *      parameters:
+     *          - name: firstname
+     *            type: string
+     *            minLength: 1
+     *            in: formData
+     *          - name: lastname
+     *            type: string
+     *            minLength: 1
+     *            in: formData
+     *          - name: email
+     *            type: string
+     *            format: email
+     *            in: formData
+     *          - name: password
+     *            type: string
+     *            minLength: 8
+     *            format: password
+     *            in: formData
+     *          - name: phone
+     *            type: string
+     *            minLength: 10
+     *            maxLength: 10
+     *            in: formData
+     *          - name: referral
+     *            type: string
+     *            in: formData
+     *      responses:
+     *          '200':
+     *              description: account sucessfully created
+     *          '422':
+     *              description: invalid or missing body elements
+     *          '500':
+     *              description: user already exists
+     *        
+     */
     app.post('/api/accounts/register', accounts.validate('register'), accounts.register);
 
-    // activate a newly created account
+    /**
+     * @swagger
+     * /api/accounts/activateAccount:
+     *  post:
+     *      tags:
+     *          - Accounts
+     *      name: Activate
+     *      summary: Activates a new account
+     *      produces:
+     *          - application/json
+     *      consumes:
+     *          - application/x-www-form-urlencoded
+     *      parameters:
+     *          - name: token
+     *            type: string
+     *            in: formData
+     *      responses:
+     *          '200':
+     *              description: account sucessfully verified
+     *          '422':
+     *              description: invalid token/email; token expired; account already verified
+     *          '500':
+     *              description: error with database communication
+     *        
+     */
     app.post('/api/accounts/activateAccount', accounts.activate);
 
-    // log into an account
+    /**
+     * @swagger
+     * /api/accounts/login:
+     *  post:
+     *      tags:
+     *          - Accounts
+     *      name: Login
+     *      summary: Organizer login
+     *      produces:
+     *          - application/json
+     *      consumes:
+     *          - application/x-www-form-urlencoded
+     *      parameters:
+     *          - name: email
+     *            type: string
+     *            format: email
+     *            in: formData
+     *          - name: password
+     *            type: string
+     *            minLength: 8
+     *            format: password
+     *            in: formData
+     *      responses:
+     *          '200':
+     *              description: account successfully logged in
+     *          '422':
+     *              description: invalid body elements or unverified account
+     *          '500':
+     *              description: error generating session token
+     *        
+     */
     app.post('/api/accounts/login', accounts.validate('login'), accounts.login);
 
-    // get user based on token
+    /**
+     * @swagger
+     * /api/accounts/getUser:
+     *  post:
+     *      tags:
+     *          - Accounts
+     *      name: user details
+     *      summary: get user details based on token
+     *      produces:
+     *          - application/json
+     *      consumes:
+     *          - application/x-www-form-urlencoded
+     *      parameters:
+     *          - name: token
+     *            type: string
+     *            minLength: 1
+     *            in: formData
+     *      responses:
+     *          '200':
+     *              description: valid token; returns userId/email
+     *          '422':
+     *              description: invalid token
+     *        
+     */
     app.post('/api/accounts/getUser', accounts.getUser);
 }
