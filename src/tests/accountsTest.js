6df40f980d0a1ffb37d4650abc5c736f8f9e98ef
Brian Wan <bwan1224@yahoo.com>
2019-07-15 22:56:54 -0400

    this actually worksssss; probably wayy too overcomplicated for this

--- src/tests/accountsTest.js
+++ src/tests/accountsTest.js
@@ -9,6 +9,7 @@ const chaiHttp = require('chai-http');
 const app = require('../../server');
 const jquerycsv = require('jquery-csv');
 const fs = require('fs');
+const async = require('async');
 
 // configure chai
 chai.use(chaiHttp);
@@ -16,41 +17,24 @@ chai.use(chaiHttp);
 // extends objects with should for easier assertions
 chai.should();
 
-// helper function to read csv files
-// toArrays returns array of arrays with headers as first array
-function readFile(filepath){
-  fs.readFile(filepath, 'UTF-8', function(err, csv){
-    if(err) console.log(err);
-    jquerycsv.toArrays(csv, {}, function (err, data) {
-      if (err) { console.log(err); }
-      //console.log(data);
-      return data;
-    });
-  });
-}
-
 describe("Accounts", () => {
   fs.readFile('./src/tests/testData/register-unit.csv', 'UTF-8', function(err, csv){
     if(err) console.log(err);
     jquerycsv.toArrays(csv, {}, function (err, csvData) {
       if (err) { console.log(err); }
       var csvHeaders = csvData[0];
-      for(var i = 1; i < csvData.length; i++){
-        var firstname = csvData[i][csvHeaders.indexOf('firstname')];
-        var lastname = csvData[i][csvHeaders.indexOf('lastname')];
-        var email = csvData[i][csvHeaders.indexOf('email')];
-        var password = csvData[i][csvHeaders.indexOf('password')];
-        var phone = csvData[i][csvHeaders.indexOf('phone')];
-        var status = parseInt(csvData[i][csvHeaders.indexOf('status')]);
-
+      // remove headers
+      csvData.shift();
+      async.each(csvData, function(csvRow, callback){
         describe("POST /api/accounts/register", () =>{
           it("register unit tests", (done) => {
-            console.log('firstname: ' + firstname);
-            console.log('lastname: ' + lastname);
-            console.log('email: ' + email);
-            console.log('password: ' + password);
-            console.log('phone: ' + phone);
-            console.log('status: ' + status);
+            console.log(csvRow);
+            var firstname = csvRow[csvHeaders.indexOf('firstname')];
+            var lastname = csvRow[csvHeaders.indexOf('lastname')];
+            var email = csvRow[csvHeaders.indexOf('email')];
+            var password = csvRow[csvHeaders.indexOf('password')];
+            var phone = csvRow[csvHeaders.indexOf('phone')];
+            var status = parseInt(csvRow[csvHeaders.indexOf('status')]);
             chai.request(app)
             .post('/api/accounts/register')
             .set('content-type', 'application/x-www-form-urlencoded')
@@ -72,7 +56,7 @@ describe("Accounts", () => {
             });
           }) //it
         }); //describe
-      } // for
+      });
     });
   });
 });
