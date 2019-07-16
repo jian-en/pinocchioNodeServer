9e129e9bd1e3de9e09ba895dcf651b11191d894b
Brian Wan <bwan1224@yahoo.com>
2019-07-16 16:09:42 -0400

    added dev/test envs again; creates entire test env based on npm test

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,18 @@
-npm run delete-tables
+# point to where test dynamodb is
+export NPM_TEST="http://localhost:8020"
+
+# start test instance
+docker-compose -f docker-test.yml up -d
+
+# create/delete tables in test db
 npm run create-tables
+
+# load test data
 node src/tests/testData/loadData.js
+
+# run tests
 mocha --timeout 10000 --exit 'src/tests/*.js'
+
+# cleanup
 npm run delete-tables
+docker-compose -f docker-test.yml down
