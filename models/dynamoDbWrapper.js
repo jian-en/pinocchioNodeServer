837fa1161cf6f8b834b67881e8670f14cc4b8600
fujian <fujian_en@126.com>
2019-07-18 15:02:18 -0400

    Init interfaces

--- models/dynamoDbWrapper.js
+++ models/dynamoDbWrapper.js
@@ -11,59 +11,59 @@ AWS.config.update(config[env].dynamodb);
 var dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.scanData = async (tableName) => {
-  try {
-    const params = {TableName: tableName};
-    const data = await dynamoDb.scan(params).promise();
-    return {success: true, data: data.Items};
-  } catch(err) {
-    console.error(err);
-    return {success: false, ...err}
-  }
+    try {
+        const params = {TableName: tableName};
+        const data = await dynamoDb.scan(params).promise();
+        return {success: true, data: data.Items};
+    } catch(err) {
+        console.error(err);
+        return {success: false, ...err}
+    }
 };
 
 module.exports.queryData = async (tableName, args) => {
-  try {
-    const params = {TableName: tableName, ...args};
-    const data = await dynamoDb.query(params).promise();
-    return {success: true, data: data.Items};
-  } catch(err) {
-    console.error(err);
-    return {success: false, ...err}
-  }
+    try {
+        const params = {TableName: tableName, ...args};
+        const data = await dynamoDb.query(params).promise();
+        return {success: true, data: data.Items};
+    } catch(err) {
+        console.error(err);
+        return {success: false, ...err}
+    }
 };
 
 module.exports.putData = async (tableName, item) => {
-  try {
-    const params = {TableName: tableName, Item: item};
-    await dynamoDb.put(params).promise();
-    return {success: true};
-  } catch(err) {
-    console.error("error when putting data - ", item);
-    console.error(err);
-    return {success: false, ...err}
-  }
+    try {
+        const params = {TableName: tableName, Item: item};
+        await dynamoDb.put(params).promise();
+        return {success: true};
+    } catch(err) {
+        console.error("error when putting data - ", item);
+        console.error(err);
+        return {success: false, ...err}
+    }
 }
 
 // TODO ID should be unique ...
 module.exports.generateID = () => {
-  return Math.random().toString();
+    return Math.random().toString();
 };
 
 module.exports.getUser = async (email) => {
-  const args = {
-    IndexName: 'emailIndex',
-    KeyConditionExpression: 'email= :e',
-    ExpressionAttributeValues: {":e": email}
-  };
-  return await this.queryData('usersTable', args);
+    const args = {
+        IndexName: 'emailIndex',
+        KeyConditionExpression: 'email= :e',
+        ExpressionAttributeValues: {":e": email}
+    };
+    return await this.queryData('usersTable', args);
 };
 
 
 ////// HELPERS
 module.exports.getDateString = (dateObj) => {
-  return dateObj.toISOString();
+    return dateObj.toISOString();
 };
 
 module.exports.getDateObj = (dateStr) => {
-  return new Date(dateStr);
+    return new Date(dateStr);
 };
