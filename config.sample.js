5a44701136b31ea0f8f0bcdec5460d826b529803
fujian <fujian_en@126.com>
2019-07-22 15:44:41 -0400

    Add swagger URL

--- config.sample.js
+++ config.sample.js
@@ -1,47 +1,49 @@
 const env = process.env.NODE_ENV || 'dev';
 
 const dev = {
- dynamodb: {
-   accessKeyId: 'foo',
-   secretAccessKey: 'bar',
-   region: 'local',
-   endpoint: 'http://dynamodb-dev-container:8000',
- },
- dynamoPublicEndpoint: 'http://localhost:8000',
- jwtSecret: 'pinocchioxdata61',
- mailAuth: {
-   user: 'xxx@gmail.com',
-   password: 'password'
- },
- reactServer: 'http://localhost:3000'
+  dynamodb: {
+    accessKeyId: 'foo',
+    secretAccessKey: 'bar',
+    region: 'local',
+    endpoint: 'http://dynamodb-dev-container:8000',
+  },
+  dynamoPublicEndpoint: 'http://localhost:8000',
+  jwtSecret: 'pinocchioxdata61',
+  mailAuth: {
+    user: 'xxx@gmail.com',
+    password: 'password'
+  },
+  reactServer: 'http://localhost:3000',
+  swaggerURL: 'http://localhost:3000'
 };
 
 const test = {
- dynamodb: {
-   accessKeyId: 'foo',
-   secretAccessKey: 'bar',
-   region: 'local',
-   endpoint: process.env.NPM_TEST || 'http://dynamodb-test-container:8000',
- },
- dynamoPublicEndpoint: 'http://localhost:8020',
- jwtSecret: 'pinocchioxdata61InTest',
- mailAuth: {
-   user: 'xxx@gmail.com',
-   password: 'password'
- }
+  dynamodb: {
+    accessKeyId: 'foo',
+    secretAccessKey: 'bar',
+    region: 'local',
+    endpoint: process.env.NPM_TEST || 'http://dynamodb-test-container:8000',
+  },
+  dynamoPublicEndpoint: 'http://localhost:8020',
+  jwtSecret: 'pinocchioxdata61InTest',
+  mailAuth: {
+    user: 'xxx@gmail.com',
+    password: 'password'
+  }
 };
 
 const awstest = {
-    dynamodb: {
-        region: 'us-east-1',
-    },
-    dynamoPublicEndpoint: 'http://localhost:8020',
-    jwtSecret: 'pinocchioxdata61InTest',
-    mailAuth: {
-        user: 'xxx@gmail.com',
-        password: 'password'
-    },
-    reactServer: 'http://localhost:3000'
+  dynamodb: {
+    region: 'us-east-1',
+  },
+  dynamoPublicEndpoint: 'http://localhost:8020',
+  jwtSecret: 'pinocchioxdata61InTest',
+  mailAuth: {
+    user: 'xxx@gmail.com',
+    password: 'password'
+  },
+  reactServer: '',
+  swaggerURL: 'http://3.92.34.83:3010'
 };
 const config = {
   dev,
