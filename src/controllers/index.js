9b62edb2461176b5b07ef33150a00b70fc520948
fujian <fujian_en@126.com>
2019-07-23 15:37:02 -0400

    Mute the logs

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,8 @@
 controller for index.js
 */
 
-const config = require('../../config.js');
 
 // welcome
 exports.welcome = (req, res) => {
-  console.log(config);
   res.send('Hello World!\n');
 };
