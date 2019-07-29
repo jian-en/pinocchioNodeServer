2176aa2e1976562c4a1d5d124afa1825ab0d2cfa
fujian <fujian_en@126.com>
2019-07-29 18:39:33 -0400

    add new repo

--- codedeploy/build_docker.sh
+++ codedeploy/build_docker.sh
@@ -2,4 +2,5 @@
 # this is used by codedeploy
 # Dockerfile and tag it as 'server:test'
 cp /server/config.sample.js /server/config.js
+git clone https://github.com/jian-en/pinocchioContract.git smart_contracts/contracts
 docker build -t server:test /server
