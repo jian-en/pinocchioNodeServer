42030195cb2797547635e2a8af7ca7a760b81e9a
fujian <fujian_en@126.com>
2019-07-29 14:33:48 -0400

    Add more configs

--- config.sample.js
+++ config.sample.js
@@ -15,7 +15,12 @@ const dev = {
     password: 'password'
   },
   reactServer: 'http://localhost:3000',
-  swaggerURL: 'localhost:3000'
+  swaggerURL: 'localhost:3000',
+  providerURI: 'http://127.0.0.1:7545',
+  publicKey: '0x976bcD111D081c50E70D8e51f27E1E08782E73cD',
+  contractAddress: '0xb25AF8C3E64500008faa3F0F8379395B8910fF86',
+  contractAbi: './contracts/local/BallotManager.json',
+  privateKey: '5d524f41a86dd946dcc654a4fb8bd4a70d9bcd6325a72ba2f495fa87bcdbb6ee'
 };
 
 const test = {
@@ -39,14 +44,18 @@ const awstest = {
   },
   dynamoPublicEndpoint: 'http://localhost:8020',
   jwtSecret: 'to-be-populated',
-  privateKey: 'to-be-populated',
   mailAuth: {
     user: 'xxx@gmail.com',
     password: 'to-be-populated'
   },
   reactServer: 'http://3.92.34.83:3010',
   swaggerURL: '3.92.34.83:3010',
-  secretName: 'test/pinocchioNodeServer/aws'
+  secretName: 'test/pinocchioNodeServer/aws',
+  providerURI: 'http://3.92.34.83:8545', // Need to figure out
+  publicKey: 'to-be-filled',
+  contractAddress: 'to-be-filled',
+  contractAbi: './contracts/test/BallotManager.json',
+  privateKey: 'to-be-populated'
 };
 
 const config = {
